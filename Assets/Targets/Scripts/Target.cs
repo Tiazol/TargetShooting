14ccb0716515560173adcbf3eaf13aa59e72c175
Vasilii Makarov <36786006+Tiazol@users.noreply.github.com>
2021-05-10 12:22:34 +0300

    Fix that target destroying doesn't disable target collider

--- Assets/Targets/Scripts/Target.cs
+++ Assets/Targets/Scripts/Target.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(ParticleSystem), typeof(SpriteRenderer))]
+[RequireComponent(typeof(ParticleSystem), typeof(SpriteRenderer), typeof(Collider2D))]
 public class Target : MonoBehaviour
 {
     ParticleSystem particles;
     SpriteRenderer spriteRenderer;
+    Collider2D collider2d;
 
     public event System.Action Destroying;
 
@@ -12,11 +13,13 @@ public class Target : MonoBehaviour
     {
         particles = GetComponent<ParticleSystem>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2d = GetComponent<Collider2D>();
     }
 
     public void Destroy()
     {
         spriteRenderer.enabled = false;
+        collider2d.enabled = false;
         particles.Play();
         Destroying?.Invoke();
         Destroy(gameObject, particles.main.duration + particles.main.startLifetimeMultiplier);
